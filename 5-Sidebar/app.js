31acb98c1fa23a4c4c825f2fb244006214a11194
aniruddhathorat123 <aniruddhathorat123@gmail.com>
2022-07-14 19:26:04 +0530

    Added `Navbar` and `Sidebar` projects.

--- 5-Sidebar/app.js
+++ 5-Sidebar/app.js
@@ -2,7 +2,7 @@ const toggleBtn = document.querySelector(".sidebar-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
-toggleBtn.addEventListener("hover", function() {
+toggleBtn.addEventListener("click", function() {
     console.log(sidebar.classList);
     // if (sidebar.classList.contains("show-sidebar")) {
     //     sidebar.classList.remove("show-sidebar");
